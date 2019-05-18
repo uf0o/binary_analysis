4f3b521da66307f517176ee2ab1c28e644617126
Matteo Malvica <matteo@malvica.com>
2019-05-18 17:09:09 +0200

    Create ep_patcher.py

---
+++ ep_patcher.py
@@ -0,0 +1,14 @@
+#quick and dirth ELF entrypoint patcher
+
+import os
+import sys
+import binascii
+
+# usage: ep_patcher.py -filename -new_entrypoint
+patch_file_input = sys.argv[1]
+new_ep = sys.argv[2]
+new_ep = binascii.unhexlify(new_ep)
+
+with open(patch_file_input, 'rb+') as f:
+    f.seek(24)
+    f.write(new_ep)
